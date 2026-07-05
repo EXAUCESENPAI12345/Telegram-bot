2f42294d01fbf4b8b03c6bf857eebe273e40d3c6
EXAUCESENPAI12345 <mpikabeni56@gmail.com>
2026-07-05 02:16:38 +0100

    Ajouter du fichier main.py pour le bot

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot OK 🚀 (Render clean)")
+    await update.message.reply_text("Bienvenue sur NexamPay 🚀")
+
+async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Message reçu: {update.message.text}")
 
 def main():
     if not TOKEN:
-        print("❌ TOKEN manquant")
+        print("TOKEN manquant")
         return
 
     app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
 
-    print("Bot lancé 🚀")
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle))
+
+    print("NexamPay en ligne 🚀")
     app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
